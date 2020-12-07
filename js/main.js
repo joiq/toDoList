b5ea0db55cfdf783d3afecc216d9551e19c15b23
Sasha <frozeefrozee12345@gmail.com>
2020-12-07 14:13:41 +0300

    change design

--- js/main.js
+++ js/main.js
@@ -1,16 +1,64 @@
-"use strict";
+'use strict';
 
-let addButton = document.querySelector('.add-button');
-let todoList = document.querySelector('.todo-list');
-let inputValue = document.querySelector('.item-value__input');
+const inputForValueItem = document.querySelector('.item__input'),
+      buttonForAddItem = document.querySelector('.item__add'),
+      list = document.querySelector('.list');
 
-addButton.addEventListener('click', addListItem);
+let listArr = [];
+
+if (localStorage.getItem('todo')) {
+  listArr = JSON.parse(localStorage.getItem('todo'));
+  displayListItem();
+}
 
 // Добавить дело
-function addListItem() {
-    let li = document.createElement('li');
-    li.innerHTML = inputValue.value;
+buttonForAddItem.addEventListener('click', () => {
+  if (!inputForValueItem.value) {
+    return;
+  }
 
-    todoList.prepend(li);
-    inputValue.value = '';
+  let newTodo = {
+    itemName: inputForValueItem.value,
+    checked: false,
+  };
+
+  listArr.push(newTodo);
+  inputForValueItem.value = ''; 
+  displayListItem();
+  
+  localStorage.setItem('todo', JSON.stringify(listArr));
+});
+
+
+
+// Вывести дела на экран
+function displayListItem() {
+  let displayItem = '';
+
+  listArr.forEach((item, i) => {
+    displayItem += `
+    <li class="list__item">
+      <input class="list__checkbox" type="checkbox" id="item_${i}" ${item.checked ? "checked": ""}>
+      <label for="item_${i}">${item.itemName}</label>
+    </li>
+    `;
+    
+    list.innerHTML = displayItem;
+  });
 }
+
+// 
+list.addEventListener('change', function(event) {
+  let idCheckBox = event.target.getAttribute('id');
+  let valueLabel = list.querySelector('[for=' + idCheckBox +']').innerHTML;
+  
+  listArr.forEach(function(item) {
+    if (item.itemName === valueLabel) {
+      item.checked = !item.checked;
+      localStorage.setItem('todo', JSON.stringify(listArr));
+    } 
+  });
+
+  let input = event.target;
+  input.classList.add('item__done');
+});
